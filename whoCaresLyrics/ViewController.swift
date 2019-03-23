e2ccb072df895408438cee8bf700556f8c806c37
jasperlin1996 <jasperlin1996@gmail.com>
2019-03-23 13:51:01 +0800

    Initial Commit

---
+++ whoCaresLyrics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whoCaresLyrics
+//
+//  Created by Jasper Lin on 2019/3/21.
+//  Copyright © 2019 jasperlin1996. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
